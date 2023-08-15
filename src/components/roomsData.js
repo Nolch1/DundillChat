24b7207ce738b7df376cad30427b742a2e84aaf4
ghassenzaghmi <133372104+ghassenzaghmi@users.noreply.github.com>
2023-08-15 22:55:31 +0000

    Add feature XYZ

---
+++ src/components/roomsData.js
@@ -0,0 +1,13 @@
+import dundill from "../img/dun.png";
+export const roomData = [
+  { name: "Tunimillion Project", image: dundill },
+  { name: "Dundil chat project", image: dundill },
+  { name: "Dundil IT company project", image: dundill },
+  { name: "Moisons Project", image: dundill },
+  { name: "General Chat Room", image: dundill },
+  { name: "Alice ALI", image: dundill },
+  { name: "Alice Johnson", image: dundill },
+  { name: "Alice Johnson", image: dundill },
+  { name: "Alice Johnson", image: dundill },
+  { name: "Alice Johnson", image: dundill },
+];
--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import Sidebar from '../components/Sidebar';
-
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import RoomsCo from "../components/RoomsCo";
 
 const Rooms = () => {
   return (
-    <div className='myapp'>
+    <div className="myapp">
       <div className="container-fluid">
         <div className="row">
           <div className="col-lg-3 col-md-4 col-sm-12 p-0 ">
             <Sidebar />
           </div>
-          <div style={{backgroundColor:"white",borderTopLeftRadius:50,borderBottomLeftRadius:50,paddingLeft:60}} className="col-lg-9 col-md-8 col-sm-12 ">
-           
-            
-        
-            
+          <div
+            style={{
+              backgroundColor: "white",
+              borderTopLeftRadius: 50,
+              borderBottomLeftRadius: 50,
+              paddingLeft: 60,
+            }}
+            className="col-lg-9 col-md-8 col-sm-12 "
+          >
+            <RoomsCo />
           </div>
         </div>
       </div>
